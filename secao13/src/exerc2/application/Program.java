66169f51975e31388892c89f32bf39259465cf13
Thallita <thallitaspedreira@gmail.com>
2026-02-02 23:23:23 -0300

    secao 13 - exerc 1, 2 e 3

---
+++ secao13/src/exerc2/application/Program.java
@@ -0,0 +1,35 @@
+package secao13.src.exerc2.application;
+
+import secao13.src.exerc2.entities.Account;
+import secao13.src.exerc2.entities.BusinessAccount;
+import secao13.src.exerc2.entities.SavingsAccount;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Account acc = new Account(1001, "Tim", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Lucy", 0.0, 500.0);
+
+        // UPCASTING
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Celina", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Angela", 0.0, 0.01);
+
+        // DOWNCASTING
+        BusinessAccount acc4 = (BusinessAccount)acc2;
+        acc4.loan(100.0);
+
+        // BusinessAccount acc5 = (BusinessAccount)acc3;
+        if (acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
+    }
+}
