3376765ab94a468b203578fed87de5cffab203f9
Thallita <thallitaspedreira@gmail.com>
2026-02-07 22:44:58 -0300

    secao 16 - exerc 2

---
+++ secao16/src/exerc2/model/entities/Shape.java
@@ -0,0 +1,5 @@
+package secao16.src.exerc2.model.entities;
+
+public interface Shape {
+    double area();
+}
---
+++ secao16/src/exerc2/model/enums/Color.java
@@ -0,0 +1,6 @@
+package secao16.src.exerc2.model.enums;
+
+public enum Color {
+    BLACK,
+    WHITE;
+}
