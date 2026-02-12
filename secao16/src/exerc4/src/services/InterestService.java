dfb9858ce6c1a9942f69600893f09d7306cab133
Thallita <thallitaspedreira@gmail.com>
2026-02-11 21:31:26 -0300

    secao 16 - exerc 4

---
+++ secao16/src/exerc4/src/services/InterestService.java
@@ -0,0 +1,14 @@
+package secao16.src.exerc4.src.services;
+
+import java.security.InvalidParameterException;
+
+public interface InterestService {
+    double getInterestRate();
+
+    default double payment(double amount, int months) {
+        if (months < 1) {
+            throw new InvalidParameterException("Months must be greater than zero");
+        }
+        return amount * Math.pow(1.0 + getInterestRate() / 100.0, months);
+    }
+}
