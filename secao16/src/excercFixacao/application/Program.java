6d962d73bbbe3f68b3861093a851c0105e0a865b
Thallita <thallitaspedreira@gmail.com>
2026-02-16 22:41:34 -0300

    secao 16 - exerc fixação

--- secao16/src/excercFixacao/application/Program.java
+++ secao16/src/excercFixacao/application/Program.java
@@ -1,4 +1,42 @@
 package secao16.src.excercFixacao.application;
 
+import secao16.src.excercFixacao.model.entities.Contract;
+import secao16.src.excercFixacao.model.entities.Installment;
+import secao16.src.excercFixacao.model.services.ContractService;
+import secao16.src.excercFixacao.model.services.PaypalService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter contract data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Date (dd/MM/yyyy): ");
+        Date contractDate = sdf.parse(sc.next());
+        System.out.print("Contract value: ");
+        double contractValue = sc.nextDouble();
+        Contract contract = new Contract(number, contractDate, contractValue);
+
+        System.out.print("Enter number of installments: ");
+        int n = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+        contractService.processContract(contract, n);
+
+        System.out.println("Installments:");
+        for (Installment x : contract.getInstallments()) {
+            System.out.println(sdf.format(x.getDueDate()) + " - " + String.format("%.2f", x.getAmount()));
+        }
+        sc.close();
+    }
 }
---
+++ secao16/src/excercFixacao/model/services/ContractService.java
@@ -0,0 +1,32 @@
+package secao16.src.excercFixacao.model.services;
+
+import secao16.src.excercFixacao.model.entities.Contract;
+import secao16.src.excercFixacao.model.entities.Installment;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months) {
+        double basicQuota = contract.getTotalValue() / months;
+        for (int i=1; i<=months; i++) {
+            Date dueDate = addMonths(contract.getDate(), i);
+            double updatedQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+            double fullQuota = updatedQuota + onlinePaymentService.paymentFee(updatedQuota);
+            contract.addInstallment(new Installment(dueDate, fullQuota));
+        }
+    }
+
+    private Date addMonths(Date source, int n) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(source);
+        calendar.add(Calendar.MONTH, n);
+        return calendar.getTime();
+    }
+}
