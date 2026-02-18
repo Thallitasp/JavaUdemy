ebe1cb876f74e5f76728c8a9863d99b31d403193
Thallita <thallitaspedreira@gmail.com>
2026-02-17 22:07:14 -0300

    secao 16 - update

--- secao16/src/excercFixacao/application/Program.java
+++ secao16/src/excercFixacao/application/Program.java
@@ -16,15 +16,19 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         System.out.println("Enter contract data");
         System.out.print("Number: ");
         int number = sc.nextInt();
+
         System.out.print("Date (dd/MM/yyyy): ");
         Date contractDate = sdf.parse(sc.next());
+
         System.out.print("Contract value: ");
         double contractValue = sc.nextDouble();
+
         Contract contract = new Contract(number, contractDate, contractValue);
 
         System.out.print("Enter number of installments: ");
@@ -34,9 +38,11 @@ public class Program {
         contractService.processContract(contract, n);
 
         System.out.println("Installments:");
+
         for (Installment x : contract.getInstallments()) {
             System.out.println(sdf.format(x.getDueDate()) + " - " + String.format("%.2f", x.getAmount()));
         }
+
         sc.close();
     }
 }
